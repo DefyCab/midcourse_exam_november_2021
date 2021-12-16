793d17766a0e78609c1c1ef2bd792479c016ac99
DefyCab <love.bellinder@gmail.com>
2021-12-16 10:40:52 +0100

    adds spec for making calls to the API using intercept. Adds fixture

---
+++ cypress/integration/userCanMakeASearchForAUser.js
@@ -0,0 +1,12 @@
+describe('A user can search for a user on GitHub', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**api/**', {
+      fixture: './userSearch.json',
+    }).as('User.index')
+    cy.visit('/')
+  })
+
+  it('is expected to ta make a call a return status code 200', () => {
+    cy.wait('User.index').its('response.Statuscode').should('eq', 200)
+  })
+})
